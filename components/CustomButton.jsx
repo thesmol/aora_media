1449c49888f0c59309f525398fde13b2933ff82a
Медведев Иван <ranzar2000@gmail.com>
2024-04-14 20:35:06 +0300

    Reduce the height of CustomButton component

--- components/CustomButton.jsx
+++ components/CustomButton.jsx
@@ -12,7 +12,7 @@ const CustomButton = ({
         <TouchableOpacity
             onPress={handlePress}
             activeOpacity={0.7}
-            className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center ${containerStyles} ${isLoading && 'opacity-50'}`}
+            className={`bg-secondary rounded-xl min-h-[52px] justify-center items-center ${containerStyles} ${isLoading && 'opacity-50'}`}
             disabled={isLoading}
         >
             <Text className={`text-primary font-psemibold text-lg ${textStyles}`}>
