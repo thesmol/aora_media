ebe29e0783b08fd34a21097d312fc693e62e3ddc
Медведев Иван <ranzar2000@gmail.com>
2024-04-15 00:21:24 +0300

    Wrap the entire application in GlobalProvider

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -30,20 +30,23 @@ const RootLayout = () => {
     }
 
     return (
-        <Stack>
-            <Stack.Screen name='index' options={{
-                headerShown: false
-            }} />
-            <Stack.Screen name='(auth)' options={{
-                headerShown: false
-            }} />
-            <Stack.Screen name='(tabs)' options={{
-                headerShown: false
-            }} />
-            {/* <Stack.Screen name='/search/[query]' options={{
+        <GlobalProvider>
+            <Stack>
+                <Stack.Screen name='index' options={{
+                    headerShown: false
+                }} />
+                <Stack.Screen name='(auth)' options={{
+                    headerShown: false
+                }} />
+                <Stack.Screen name='(tabs)' options={{
+                    headerShown: false
+                }} />
+                {/* <Stack.Screen name='/search/[query]' options={{
                 headerShown: false
             }} /> */}
-        </Stack>
+            </Stack>
+        </GlobalProvider>
+
     )
 }
 
