b50713019b8ef236206bb3c1df0c0eca418ac209
Медведев Иван <ranzar2000@gmail.com>
2024-04-14 16:51:30 +0300

    Implement a TabIcon component for bottom tabs

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,10 +1,20 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import { Tabs } from 'expo-router'
+import { icons } from "../../constants"
 
-const TabsLayout = () => {
+const TabIcon = ({ icon, color, name, focused }) => {
     return (
-        <View>
-            <Text>TabsLayout</Text>
+        <View className="items-center justify-center gap-2">
+            <Image
+                source={icon}
+                resizeMode="contain"
+                tintColor={color}
+                className="w-6 h-6"
+            />
+            <Text className={`${focused ? 'font-psemibold' : 'font-pregular'} text-xs`} style={{ color: color }}>
+                {name}
+            </Text>
         </View>
     )
 }
