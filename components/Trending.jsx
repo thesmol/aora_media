7466e0bbf6299b65f07c476fdac46083cc28a386
Медведев Иван <ranzar2000@gmail.com>
2024-04-15 20:28:37 +0300

    Add Trending component
    The Trending displays a list of posts in a horizontal FlatList. The posts are passed as a prop to the component. Each post is displayed as a Text component with the post's ID as the text. Currently it is not ready

---
+++ components/Trending.jsx
@@ -0,0 +1,19 @@
+import { View, Text, FlatList } from 'react-native'
+import React from 'react'
+
+const Trending = ({ posts }) => {
+    return (
+        <FlatList
+            data={posts}
+            keyExtractor={(item) => item.$id}
+            renderItem={({ item }) => (
+                <Text key={item.id} className="text-white">
+                    {item.id}
+                </Text>
+            )}
+            horizontal
+        />
+    )
+}
+
+export default Trending
