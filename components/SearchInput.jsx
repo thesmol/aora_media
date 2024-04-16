8be400c1ef7b1db2a55a463772249a8802689b78
Медведев Иван <ranzar2000@gmail.com>
2024-04-16 14:23:59 +0300

    Enchance SearchInput component
    The SearchInput component uses React hooks to manage the state of the search query. It also uses the 'expo-router' package to handle navigation based on the search query. If the current path starts with '/search', it updates the query parameters; otherwise, it navigates to a new '/search' path with the query. If the search field is empty, it shows an alert asking the user to enter a search query. The search icon used in the component is imported from the 'constants' module.

--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -1,31 +1,38 @@
-import { View, TextInput, TouchableOpacity, Image } from 'react-native';
-import React from 'react';
+import { View, TextInput, TouchableOpacity, Image, Alert } from 'react-native';
+import React, { useState } from 'react';
 import { icons } from "../constants";
+import { router, usePathname } from 'expo-router';
 
-const SearchInput = ({
-    title,
-    value,
-    handleChangeText,
-    placeholder = "Искать видео топик",
-    type,
-    ...props
-}) => {
+const SearchInput = ({ initialQuery }) => {
+    const pathName = usePathname();
+    const [query, setQuery] = useState(initialQuery || "");
 
     return (
         <View className="w-full h-14 flex-row px-4 bg-black border-2 border-black-200 rounded-2xl focus:border-secondary items-center space-x-4">
             <TextInput
                 className="flex-1 text-white font-pregular text-base mt-0.5 items-center"
-                placeholder={placeholder}
-                value={value}
-                placeholderTextColor="#7b7b8b"
-                onChange={handleChangeText}
-                {...props}
-                style = {{
+                placeholder={"Искать..."}
+                value={query}
+                placeholderTextColor="#cdcde0"
+                onChangeText={(e) => setQuery(e)}
+                style={{
                     alignItems: 'center'
                 }}
             />
 
-            <TouchableOpacity>
+            <TouchableOpacity
+                onPress={() => {
+                    if (!query) {
+                        return Alert.alert("Отсутсвует запрос", "Пожалуйста введите ваш запрос для поиска")
+                    }
+
+                    if (pathName.startsWith('/search')) {
+                        router.setParams({ query });
+                    } else {
+                        router.push(`/search/${query}`);
+                    }
+                }}
+            >
                 <Image
                     className="h-6 w-6"
                     resizeMode={'contain'}
